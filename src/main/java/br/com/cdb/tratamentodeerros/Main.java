0a00f8cb1289868654c7c286884181e3fef98bb9
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-03 10:39:40 -0300

    feat: Custom Exception in Main :rocket:

--- src/main/java/br/com/cdb/tratamentodeerros/Main.java
+++ src/main/java/br/com/cdb/tratamentodeerros/Main.java
@@ -5,22 +5,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numero = 10;
+//        int numero = 10;
+//
+//        int divisor = input.nextInt();
+//        // Tratamento de erro com try-catch
+//        try {
+//            // Tentativa de dividir por zero
+//            int resultado = numero / divisor;
+//            System.out.println("Resultado: " + resultado);
+//        } catch (ArithmeticException | NullPointerException e) {
+//            // Tratamento de erro para divisão por zero
+//            System.out.println("Erro: Divisão por zero não é permitida." + e.getMessage());
+//        } finally {
+//            // Bloco finally sempre executado
+//            System.out.println("Operação finalizada.");
+//        }
 
-        int divisor = input.nextInt();
-        // Tratamento de erro com try-catch
+        double resultado = 0;
         try {
-            // Tentativa de dividir por zero
-            int resultado = numero / divisor;
-            System.out.println("Resultado: " + resultado);
-        } catch (ArithmeticException | NullPointerException e) {
+            resultado = divisao(10, 2);
+        } catch (ArithmeticException e) {
             // Tratamento de erro para divisão por zero
-            System.out.println("Erro: Divisão por zero não é permitida." + e.getMessage());
+            System.out.println("Erro: " + e.getMessage());
+
         } finally {
-            // Bloco finally sempre executado
-            System.out.println("Operação finalizada.");
+
         }
 
+
+    }
+
+    private static double divisao(int dividendo, int divisor) throws ArithmeticException {
+        double result = 0;
+        if (divisor == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida.");
+        }
+
+        result = dividendo / divisor;
+        return result;
 
     }
 }
